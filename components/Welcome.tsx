ed2917802443e5a05fb985fba7ba9972044338a0
Rafaela Medina <rafaelamedinax@gmail.com>
2023-02-26 04:00:34 -0300

    feat: apply scrollView

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,30 +1,30 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 
 export default function Welcome() {
     return(
-        <View style={{ flex: 1 }}>
-        <Text
-          style={{
-            padding: 40,
-            fontSize: 30,
-            color: '#EDEFEE',
-            textAlign: 'center',
-          }}>
-          Welcome to Little Lemon
-        </Text>
-        <Text
-          style={{
-            fontSize: 24,
-            padding: 20,
-            marginVertical: 8,
-            color: '#EDEFEE',
-            textAlign: 'center',
-          }}>
-          Little Lemon is a charming neighborhood bistro that serves simple food
-          and classic cocktails in a lively but casual environment. We would love
-          to hear your experience with us!
-        </Text>
-      </View>
+      <ScrollView indicatorStyle={"white"} style={{ flex: 1 }}>
+      <Text
+        style={{
+          padding: 40,
+          fontSize: 50,
+          color: '#EDEFEE',
+          textAlign: 'center',
+        }}>
+        Welcome to Little Lemon
+      </Text>
+      <Text
+        style={{
+          fontSize: 38,
+          padding: 20,
+          marginVertical: 8,
+          color: '#EDEFEE',
+          textAlign: 'center',
+        }}>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear more about your experience with us!
+      </Text>
+    </ScrollView>
     )
 }
